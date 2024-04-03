32341f4bee7c2043c9e23f5702e2cfbd1c033ce4
Vpx2104 <162385592+Vpx2104@users.noreply.github.com>
2024-04-03 00:51:36 +0000

    initial

---
+++ exercicio2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+ {
+    int x,y; 
+    printf("escreva o numero x: ");
+    scanf("%d", &x);
+
+    printf("escreva o numero y: ");
+    scanf("%d", &y);
+
+    printf("numero x é maior que y");
+    printf("%d\n", x>y);
+    printf("numero x é menor que y");
+    printf("%d\n", x<y);
+    printf("numero x é igual");
+    printf("%d\n", x==y;)
+    return 0;
+}
+
+//numero x maior que y = x>y
+//numero x menor que y = x<y
+//numero x igual que y = x==y
